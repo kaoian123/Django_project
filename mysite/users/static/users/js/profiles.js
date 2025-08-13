48305d8472391a1d98e2617a08f07186198f7116
Ian_Kao <ian_kao@megdeMacBook-Pro.local>
2025-08-13 15:21:22 +0800

    加入使用 Django API 的 axios

--- mysite/users/static/users/js/profiles.js
+++ mysite/users/static/users/js/profiles.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const loadingDiv = document.getElementById('loading')
   const profileListDiv = document.getElementById('profileList')
-
+  const profileListDjangoDiv = document.getElementById('profileList_django')
+  const refreshBtn = document.getElementById('refreshBtn')
   function showLoading() {
     loadingDiv.classList.remove('d-none')
   }
@@ -48,24 +49,70 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  function renderProfiles_django(profiles) {
+    try {
+      if (!profiles || !Array.isArray(profiles)) {
+        throw new Error('資料格式錯誤')
+      }
+
+      if (profiles.length === 0) {
+        throw new Error('無公開履歷資料')
+      }
+
+      profileListDjangoDiv.innerHTML = profiles
+        .map(
+          (profile) => `
+                <div class="card profile-card" style="width: 14rem;">
+                    <a href="${
+                      profile.slug
+                    }" class="text-decoration-none text-dark">
+                        <img src="${
+                          profile.avatar || '/media/avatars/default.png'
+                        }" 
+                             class="card-img-top profile-avatar" alt="avatar">
+                        <div class="card-body">
+                            <h5 class="card-title">${profile.masked_name}</h5>
+                            <p class="card-text">${
+                              profile.education || '未填寫學歷'
+                            }</p>
+                        </div>
+                    </a>
+                </div>
+            `
+        )
+        .join('')
+    } catch (error) {
+      profileListDiv.innerHTML = `<p class="text-danger">${error.message}</p>`
+    }
+  }
+
   async function main() {
     showLoading()
-    const API_URL = '/api/public-profiles/'
-    fetchData(API_URL)
+    const profile = fetchData('/api/public-profiles/')
       .then((profiles) => {
         renderProfiles(profiles)
       })
-      .catch((error) => {
+      .catch(() => {
         profileListDiv.innerHTML = `<p class="text-danger">無法載入公開履歷資料</p>`
       })
-      .finally(() => {
-        hideLoading()
+
+    const profiles_django = fetchData('/api/original-public-profiles/')
+      .then((profiles) => {
+        renderProfiles_django(profiles)
       })
+      .catch(() => {
+        profileListDiv.innerHTML = `<p class="text-danger">無法載入公開履歷資料</p>`
+      })
+
+    await Promise.allSettled([profile, profiles_django])
+    hideLoading()
   }
 
   main()
 
-  document.getElementById('refreshBtn').addEventListener('click', () => {
+  refreshBtn.addEventListener('click', () => {
+    profileListDiv.innerHTML = ''
+    profileListDjangoDiv.innerHTML = ''
     main()
   })
 })
