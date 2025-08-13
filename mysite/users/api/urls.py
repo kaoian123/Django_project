9a713ace0ca5d8eddf621d4d6441732efba3ee14
Ian_Kao <ian_kao@megdeMacBook-Pro.local>
2025-08-13 14:10:19 +0800

    建立 Django 的 JsonResponse

--- mysite/users/api/urls.py
+++ mysite/users/api/urls.py
@@ -3,4 +3,9 @@ from users.api import views
 
 urlpatterns = [
     path("public-profiles/", views.public_profile_list, name="public-profile-list"),
+    path(
+        "original-public-profiles/",
+        views.original_public_profile_list,
+        name="original-public-profile-list",
+    ),
 ]
--- mysite/users/api/views.py
+++ mysite/users/api/views.py
@@ -3,11 +3,33 @@ from users.models import Profile
 from users.api.serializers import ProfilePublicSerializer
 from rest_framework.decorators import api_view
 
+import time
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from users.utils import mask_name
+
 
 @api_view(["GET"])
 def public_profile_list(request):
     profiles = Profile.objects.filter(is_public=True)
     profiles_serializer = ProfilePublicSerializer(profiles, many=True)
-    import time
     time.sleep(3)
     return Response(profiles_serializer.data)
+
+
+@require_http_methods(["GET"])
+def original_public_profile_list(request):
+    profiles = Profile.objects.filter(is_public=True)
+
+    profile_list = []
+    for profile in profiles:
+        profile_list.append(
+            {
+                "avatar": profile.avatar.url,
+                "masked_name": mask_name(profile.full_name or profile.user.username),
+                "education": profile.get_education_display(),
+                "slug": profile.slug,
+            }
+        )
+    time.sleep(5)
+    return JsonResponse(profile_list, safe=False)
